f88fa676830f1916c7e710cfb2629b24df17731d
Hanna <skrane4ka@gmail.com>
2022-11-24 01:45:10 +0100

    Add function To Lower

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,5 @@
 ﻿int numberA = new Random().Next(1, 10);
-Console.WriteLine(numberA)  
+Console.WriteLine(numberA); 
 int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB)
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
